5ec0f004c4c241ae217fedc9d3566b6860773090
natasilva <nata.software@hotmail.com>
2024-08-24 14:47:48 -0300

    feat: Add validação

--- src/service/group.js
+++ src/service/group.js
@@ -2,6 +2,10 @@ const groupModel = require('../model/group')
 
 class GroupService {
     async get (cnpjf) {
+        if (!cnpjf) {
+            throw new Error("cnpjf_is_empty");
+        }
+
         const result = await groupModel.getUserByCnpjf(cnpjf)
         return result
     }
